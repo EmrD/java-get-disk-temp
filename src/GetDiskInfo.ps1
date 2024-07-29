4ec0ce410e7f377c0cabfbe24a3f3b98008c8a58
Emr <127445447+EmrD@users.noreply.github.com>
2024-07-29 20:43:10 +0300

    Update GetDiskInfo.ps1

--- src/GetDiskInfo.ps1
+++ src/GetDiskInfo.ps1
@@ -1,3 +1,19 @@
 $desktopPath = [Environment]::GetFolderPath("Desktop")
 $outputFilePath = "$desktopPath\output.csv"
-Get-Disk | Get-StorageReliabilityCounter | Select-Object -Property DeviceId, Temperature | Export-Csv -Path $outputFilePath -NoTypeInformation -Force
+
+$disks = Get-Disk | Get-StorageReliabilityCounter
+
+$output = @()
+foreach ($disk in $disks) {
+    $partitions = Get-Partition -DiskNumber $disk.DeviceId
+    $diskName = $partitions | Where-Object { $_.DriveLetter } | Select-Object -First 1 -ExpandProperty DriveLetter
+    if (-not $diskName) {
+        $diskName = "Disk " + $disk.DeviceId
+    }
+    $output += [PSCustomObject]@{
+        DiskName = $diskName
+        Temperature = $disk.Temperature
+    }
+}
+
+$output | Export-Csv -Path $outputFilePath -NoTypeInformation -Force
